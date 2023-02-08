dad42f75c2de9a93c33ee74c8379f914d1e6fc97
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-02-08 14:24:50 -0800

    Working on Functionality

--- app/src/main/java/com/cjl/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/cjl/tipcalculator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.cjl.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val costOfService = findViewById<EditText>(R.id.cost_of_service)
+        val calculateButton = findViewById<Button>(R.id.calculate_button)
+
+
+        calculateButton.setOnClickListener(){
+
+            val inputPrice = costOfService.text
+
+            
+
+            Toast.makeText(this,inputPrice, Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
